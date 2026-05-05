fb96625e6f61cab014cc84fbec9ef7a93237edd3
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-05 10:49:01 +0300

    add endpoints & add types:delete and update ,

---
+++ src/features/playlists/api/playlistsApi.ts
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {
+  CreatePlaylistArgs,
+  PlaylistData,
+  PlaylistsResponse, UpdatePlaylistArgs
+} from '@/features/playlists/api/playlistsApi.types.ts'
+
+export const playlistsApi = createApi({
+  reducerPath: 'playlistsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    headers: {
+      'API-KEY': import.meta.env.VITE_API_KEY,
+    },
+    prepareHeaders: (headers) => {
+      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+      return headers
+    },
+  }),
+  endpoints: (build) => ({
+    fetchPlaylists: build.query<PlaylistsResponse, void>({
+      query: () => 'playlists',
+    }),
+    createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
+      query: (body) => ({
+        method: 'post',
+        url: 'playlists',
+        body,
+      }),
+    }),
+    deletePlaylist: build.mutation<void, string>({
+      query: (playlistId) => ({
+        method: 'delete',
+        url: `playlists/${playlistId}`,
+      }),
+    }),
+    updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
+      query: ({ playlistId ,body}) => ({
+        method: 'put',
+        url: `playlists/${playlistId}`, body
+      }),
+    }),
+  }),
+})
+export const { useFetchPlaylistsQuery,useCreatePlaylistMutation,useDeletePlaylistMutation,useUpdatePlaylistMutation } = playlistsApi
