c4e7b486c3ea7f4daef3a8b25de4dbeb179d5818
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-07 18:38:01 +0300

    create and add linearProgress

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -2,16 +2,19 @@
 
 import s from './App.module.css'
 
-import { Header } from '@/common/components'
+import { Header, LinearProgress } from '@/common/components'
 import { Routing } from '@/common/routing'
 import { ToastContainer } from 'react-toastify'
+import { useGlobalLoading } from '@/common/hooks'
 
 
 
 export const App = () => {
+  const isGlobalLouding = useGlobalLoading()
   return (
     <>
       <Header />
+      {isGlobalLouding && <LinearProgress/>}
       <div className={s.layout}>
         <Routing />
       </div>
