0796ec7ed3191202139e54784126cc6c02e7e993
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-16 19:04:13 +0300

    refactor:add withZodCatch,getNextPageParam,types

--- src/features/tracks/api/tracksApi.ts
+++ src/features/tracks/api/tracksApi.ts
@@ -1,23 +1,22 @@
 import { baseApi } from '@/app/api/baseApi.ts'
 import type { FetchTracksResponse } from '@/features/tracks/api/tracksApi.types.ts'
-
-
+import { fetchTracksResponseSchema } from '@/features/tracks/model/tracks.schemas.ts'
+import { withZodCatch } from '@/common/utils'
 
 export const tracksApi = baseApi.injectEndpoints({
-  endpoints: build => ({
+  endpoints: (build) => ({
     fetchTracks: build.infiniteQuery<FetchTracksResponse, void, string | undefined>({
       infiniteQueryOptions: {
         initialPageParam: undefined,
-        getNextPageParam: lastPage => {
+        getNextPageParam: (lastPage) => {
           return lastPage.meta.nextCursor || undefined
         },
       },
-      query: ({ pageParam }) => {
-        return {
-          url: 'playlists/tracks',
-          params: { cursor: pageParam, pageSize: 5, paginationType: 'cursor' },
-        }
-      },
+      query: ({ pageParam }) => ({
+        url: 'playlists/tracks',
+        params: { cursor: pageParam, pageSize: 5, paginationType: 'cursor' },
+      }),
+      ...withZodCatch(fetchTracksResponseSchema)
     }),
   }),
 })
