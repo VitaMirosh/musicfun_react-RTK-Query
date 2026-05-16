1f0778fee4e62893ce99cfcb15e72c5e412d6309
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-16 18:55:14 +0300

    refactor:refactoring code without changing the logic

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,25 +1,9 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { handelErrors } from '@/common/utils'
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { baseQueryWithReauth } from '@/app/api/baseQueryWithReauth.ts'
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  tagTypes: ['Playlist'],
-  baseQuery: async (args, api, extraOptions) => {
-    // await new Promise((resolve)=>setTimeout(resolve,2000))
-    const result = await fetchBaseQuery({
-      baseUrl: import.meta.env.VITE_BASE_URL,
-      headers: {
-        'API-KEY': import.meta.env.VITE_API_KEY,
-      },
-      prepareHeaders: (headers) => {
-        headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-        return headers
-      },
-    })(args, api, extraOptions)
-    if (result.error) {
-      handelErrors(result.error)
-    }
-    return result
-  },
+  tagTypes: ['Playlist', 'Auth'],
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
 })
