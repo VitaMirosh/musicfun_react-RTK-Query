e1b3d391be5a0cbd11820c8183b7042b890b1537
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-16 18:51:28 +0300

    feat:add login and logout

--- src/features/auth/api/authApi.types.ts
+++ src/features/auth/api/authApi.types.ts
@@ -1,4 +1,13 @@
-export type MeResponse = {
-  userId: string
-  login: string
+
+import * as z from 'zod'
+import { loginResponseSchema, type meResponseSchema } from '@/features/auth/model/authApiSchemas.ts'
+
+export type MeResponse = z.infer<typeof meResponseSchema>
+export type LoginResponse = z.infer<typeof loginResponseSchema>
+// Arguments
+export type LoginArgs = {
+  code: string
+  redirectUri: string
+  rememberMe: boolean
+  accessTokenTTL?: string // e.g. "3m"
 }
